d47425103a7bdb46ad8a8add2ea342a677e4cb22
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2021-01-23 21:05:05 -0300

    server

---
+++ Server.js
@@ -0,0 +1,38 @@
+const axios = require('axios')
+const cors = require('cors')
+const express = require('express')
+const key = "43f5bfb9"
+
+const app = express()
+
+app.use(BodyParser.urlencoded({ extend: false }))
+app.use(BodyParser.json())
+
+app.use(cors())
+
+/*
+app.get(`/`, async (req, res) => {
+    try {
+        const { data } = await axios(`https://api.hgbrasil.com/weather?key=${key}&city_name=${cidade},${estado}`)
+        //console.log(data)
+        //list()
+        res.send(data)
+        //return res.json(data)
+
+    } catch (error) {
+        console.log(error)
+    }
+    //return res.json(data)
+})
+*/
+app.get(`/weather/:cidade/:estado`, async (req, res) => {
+    const { data } = await axios(`https://api.hgbrasil.com/weather?key=${key}&city_name=${req.params.cidade},${req.params.estado}`)
+    //console.log(data)
+    //list()
+    res.send(data)
+
+})
+
+app.listen(8000, () => {
+    console.log('rodando: http://localhost:8000/')
+})
