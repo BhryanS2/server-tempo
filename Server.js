0e03ad9841ea967623f36915b562a0f4cc305fa6
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2021-01-24 09:36:47 -0300

    server

--- Server.js
+++ Server.js
@@ -12,23 +12,6 @@ app.use(BodyParser.json())
 
 app.use(cors())
 
-
-app.get(`/`, async (req, res) => {
-    let cidade = 'guaxupe'
-    let estado = 'MG'
-    try {
-        const { data } = await axios(`https://api.hgbrasil.com/weather?key=${key}&city_name=${cidade},${estado}`)
-        //console.log(data)
-        //list()
-        res.send(data)
-        //return res.json(data)
-
-    } catch (error) {
-        console.log(error)
-    }
-    //return res.json(data)
-})
-
 app.get(`/weather/:cidade/:estado`, async (req, res) => {
     const { data } = await axios(`https://api.hgbrasil.com/weather?key=${key}&city_name=${req.params.cidade},${req.params.estado}`)
     //console.log(data)
